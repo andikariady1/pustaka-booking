dde35f0fb5d13b1dfef3ec0e720f620b83c493a9
andikariady1 <andikariady714@gmail.com>
2021-10-02 00:00:16 +0700

    Upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama Saya <b>Andika Riady</b> 
+              Saya tinggal di daerah <b>Bekasi</b> 
+              olahraga yang saya suka adalah
+              <b>REBAHAN</b>";
+    }
+}
