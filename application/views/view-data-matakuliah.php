639f4e3ff6307e48b74b9656c42807a8db04d82f
andikariady1 <andikariady714@gmail.com>
2021-11-11 16:39:36 +0700

    Halaman Login

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -40,8 +40,7 @@
             </tr>
             <tr>
                 <td colspan="3" align="center">
-                    <a href="<? base_url('matakuliah');
-                                ?>">Kembali</a>
+                    <a href="<?= base_url('matakuliah'); ?>">Kembali</a>
                 </td>
             </tr>
         </table>
