bee5893cebbc379d39b0eebdf8a826cbf36dc13c
andikariady1 <andikariady714@gmail.com>
2021-11-25 17:02:23 +0700

    PERTEMUAN 10

--- application/views/user/ubah-profile.php
+++ application/views/user/ubah-profile.php
@@ -13,7 +13,8 @@
             <div class="form-group row">
                 <label for="nama" class="col-sm-2 col-formlabel">Nama Lengkap</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nama" name="nama" value="<?= $user['nama']; ?>"><?= form_error('nama', '<small class="textdanger pl-3">', '</small>'); ?>
+                    <input type="text" class="form-control" id="nama" name="nama" value="<?= $user['nama']; ?>">
+                    <?= form_error('nama', '<small class="textdanger pl-3">', '</small>'); ?>
                 </div>
             </div>
             <div class="form-group row">
@@ -21,7 +22,7 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="imgthumbnail" alt="">
+                            <img src="<?= base_url('assets/img/profile/aku.jpg') . $user['image']; ?>" class="imgthumbnail" alt="">
                         </div>
                         <div class="col-sm-9">
                             <div class="custom-file">
@@ -32,6 +33,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="form-group row justify-content-end">
                 <div class="col-sm-10">
                     <button type="submit" class="btn btnprimary">Ubah</button>
