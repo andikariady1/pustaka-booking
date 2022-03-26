205f6036d11776a9c9f0cbaf9d1fa2aaa127ac2a
andikariady1 <andikariady714@gmail.com>
2022-03-26 16:54:34 +0700

    Pertemuan 2

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -16,31 +16,28 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="<?= base_url(); ?>">Pustaka</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" datatoggle="collapse" data-target="#navbarNavAltMarkup" ariacontrols="navbarNavAltMarkup" aria-expanded="false" arialabel="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda <span class="sronly">(current)</span></a>
+
                     <?php
                     if (!empty($this->session->userdata('email'))) { ?>
-                        <a class="nav-item nav-link" href="#">Booking Buku</a>
-
+                        <a class="nav-item nav-link" href="<?= base_url('booking'); ?> ">Booking Buku </a>
                         <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
-                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw falogin"></i> Log out</a>
-
+                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
                     <?php } else { ?>
 
                         <a class="nav-item nav-link" data-toggle="modal" datatarget="#daftarModal" href="#"><i class="fas fw fa-login"></i> Daftar</a>
-
                         <a class="nav-item nav-link" data-toggle="modal" datatarget="#loginModal" href="#"><i class="fas fw fa-login"></i> Log in</a>
 
                     <?php } ?>
-                    <span class="nav-item nav-link navright" style="display:block; marginleft:20px;">Selamat Datang <b><?= $user; ?></b></span>
+                    <span class="nav-item nav-link nav-right" style="display:block; margin-left:20px;">Selamat Datang <b><?= $user; ?></b></span>
                 </div>
             </div>
+        </div>
     </nav>
-    <div class="container mt-5"></div>
-
-</body>
+    <div class="container mt-5">
